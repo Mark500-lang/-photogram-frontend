084e91a6e61f1c9c20c44e1f892d8dc670fa9a89
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-03-30 10:29:26 +0300

    feed route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import LoginSignup from './components/login/LoginSignup';
+import Profile from './components/Profile';
+import CreatePost from './components/CreatePost';
+import EditProfile from './components/EditProfile';
+import Search from './components/Search';
 import Feed from './components/Feed';
 
 function App() {
   return (
-    <div className="App">
-      <Feed />
-    </div>
+    <BrowserRouter>
+      <Routes >
+            <Route path="/" element={<Header/>}>
+            <Route path='/search' element={<Search/>}/>
+            <Route index element={<Feed/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path="/profile/create-post" element={<CreatePost/>} />
+            <Route path="/profile/edit-profile" element={<EditProfile/>} />
+            </Route >
+          </Routes>
+    </BrowserRouter>
   );
 }
 

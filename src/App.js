c03b00bc77ba3e654b48b270fc7049138ea7e0f7
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-03-30 09:48:38 +0300

    resolve branch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from './components/Header';
-import LoginSignup from './components/login/LoginSignup';
-import Profile from './components/Profile';
-import CreatePost from './components/CreatePost';
-import EditProfile from './components/EditProfile';
-import Search from './components/Search';
+import Feed from './components/Feed';
 
-function App(){
-  return(
-    <BrowserRouter>
-          <Routes >
-            <Route path="/" element={<Header/>}>
-              <Route path='/search' element={<Search/>}/>
-              <Route index element={<LoginSignup/>}/>
-              <Route path='/profile' element={<Profile/>}/>
-              <Route path="/profile/create-post" element={<CreatePost/>} />
-              <Route path="/profile/edit-profile" element={<EditProfile/>} />
-            </Route >
-          </Routes>
-      </BrowserRouter>
-  )
-    
+function App() {
+  return (
+    <div className="App">
+      <Feed />
+    </div>
+  );
 }
-export default App;
-/*<LoginSignup/>*/
-/*
 
-    <Profile/>
- */
+export default App;
